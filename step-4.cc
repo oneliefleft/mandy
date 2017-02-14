678b4acbbbd6c7d9ccd37662862fcd2106666633
tyoung <oneliefleft@gmail.com>
2017-02-14 10:05:33 +0100

    Create initial grid.

--- step-4.cc
+++ step-4.cc
@@ -23,6 +23,9 @@
 #include <deal.II/grid/tria_iterator.h>
 #include <deal.II/grid/grid_out.h>
 
+#include <deal.II/distributed/tria.h>
+#include <deal.II/distributed/grid_refinement.h>
+
 #include <deal.II/dofs/dof_handler.h>
 #include <deal.II/dofs/dof_accessor.h>
 #include <deal.II/dofs/dof_tools.h>
@@ -62,25 +65,87 @@ namespace mandy
      */
     ~LinearElasticity ();
 
+    /**
+     * Wrapper function, that controls the order of excecution.
+     */
     void run ();
     
   private:
 
     /**
+     * Make intial coarse grid.
+     */
+    void make_coarse_grid ();
+    
+    /**
+     * MPI communicator.
+     */
+    MPI_Comm mpi_communicator;
+
+    /**
+     * A distributed grid on which all computations are done.
+     */
+    dealii::parallel::distributed::Triangulation<dim> triangulation;
+
+    /**
+     * Scalar DoF handler primarily used for interpolating material
+     * identification.
+     */
+    dealii::DoFHandler<dim> dof_handler_scalar;
+
+    // /**
+    //  * Scalar valued finite element primarily used for interpolating
+    //  * material iudentification.
+    //  */
+    // dealii::FESystem<dim> fe_scalar;
+
+    /**
+     * Index set of locally owned DoFs.
+     */
+    dealii::IndexSet locally_owned_dofs;
+
+    /**
+     * Index set of locally relevant DoFs.
+     */
+    dealii::IndexSet locally_relevant_dofs;
+
+    /**
+     * Parallel iostream.
+     */
+    dealii::ConditionalOStream pcout;
+
+    /**
+     * Stop clock.
+     */
+    dealii::TimerOutput timer;
+    
+    /**
      * Input parameter file.
      */
     dealii::ParameterHandler parameters;
     
-  };
+  }; // LinearElasticity
 
+  
   /**
    * Class constructor.
    */
   template <int dim>
   LinearElasticity<dim>::LinearElasticity (const std::string &prm_file)
-  // :
-  // dof_handler (triangulation),
-  // fe (dealii::FE_Q<dim>(2), dim)
+    :
+    mpi_communicator (MPI_COMM_WORLD),
+    triangulation (mpi_communicator,
+                   typename dealii::Triangulation<dim>::MeshSmoothing
+                   (dealii::Triangulation<dim>::smoothing_on_refinement |
+                    dealii::Triangulation<dim>::smoothing_on_coarsening)),
+    dof_handler_scalar (triangulation),
+    // Other initialisations go here.
+    pcout (std::cout,
+           (dealii::Utilities::MPI::this_mpi_process (mpi_communicator) == 0)),
+    timer (mpi_communicator,
+	   pcout,
+	   dealii::TimerOutput::summary,
+	   dealii::TimerOutput::wall_times)
   {
     parameters.declare_entry ("Global mesh refinement steps", "5",
                               dealii::Patterns::Integer (0, 20),
@@ -93,6 +158,7 @@ namespace mandy
     
     parameters.parse_input (prm_file);
   }
+
   
   /**
    * Class destructor.
@@ -100,26 +166,55 @@ namespace mandy
   template <int dim>
   LinearElasticity<dim>::~LinearElasticity ()
   {
-    // dof_handler.clear ();
+    // Wipe DoF handlers.
+    dof_handler_scalar.clear ();
   }
-  
+
+
+  /**
+   * Make initial coarse grid.
+   */
   template <int dim>
-  void LinearElasticity<dim>::run ()
+  void
+  LinearElasticity<dim>::make_coarse_grid ()
   {
+    dealii::TimerOutput::Scope t (timer, "make coarse grid");
+
+    // Create a coarse grid according to the parameters given in the
+    // input file.
+    dealii::GridGenerator::hyper_cube (triangulation, -10, 10);
+    
+    triangulation.refine_global (parameters.get_integer ("Global mesh refinement steps"));
+  }
+
+
+  /**
+   * Run the application in the order specified.
+   */
+  template <int dim>
+  void
+  LinearElasticity<dim>::run ()
+  {
+    make_coarse_grid ();
   }
   
 } // namespace ewalena
 
 
-
-
-int main ()
+/**
+ * Main function: Initialise problem and run it.
+ */
+int main (int argc, char *argv[])
 {
+  // Initialise MPI
+  dealii::Utilities::MPI::MPI_InitFinalize mpi_initialization (argc, argv, 1);
+  
   try
     {
       mandy::LinearElasticity<2> linear_elasticity ("step-4.prm");
       linear_elasticity.run ();
     }
+
   catch (std::exception &exc)
     {
       std::cerr << std::endl << std::endl
@@ -133,6 +228,7 @@ int main ()
 
       return 1;
     }
+
   catch (...)
     {
       std::cerr << std::endl << std::endl
